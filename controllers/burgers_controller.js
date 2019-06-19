2f5d87a30fe271d9cedfbd3d65a17f5d6510f84c
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-06-18 21:38:13 -0700

    fixed untracked files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,12 @@
-var express = require("express")
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get()
+
+router.
+
+
+module.exports = router;
